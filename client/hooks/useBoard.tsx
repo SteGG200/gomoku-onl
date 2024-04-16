233d3b3b4ce6acf21efc8504c8a1e5bbacb46093
Nguyen Hoa Binh <steven.nguyenhoabinh@gmail.com>
2024-04-16 17:37:14 +0700

    Fix the rule

--- client/hooks/useBoard.tsx
+++ client/hooks/useBoard.tsx
@@ -22,10 +22,10 @@ export function useBoard() : [string[][], (i : number, j: number, value: string)
 		for(let i = 0; i < board.length; i++){
 			for(let j = 0; j < board[i].length; j++){
 				if(board[i][j] === "") continue;
-				if(i + 4 < board.length && board[i][j] === board[i + 1][j] && board[i + 1][j] === board[i + 2][j] && board[i + 2][j] === board[i + 3][j] && board[i + 4][j]) return board[i][j];
-				if(j + 4 < board[i].length && board[i][j] === board[i][j + 1] && board[i][j + 1] === board[i][j + 2] && board[i][j + 2] === board[i][j + 3] && board[i][j + 4]) return board[i][j];
-				if(i + 4 < board.length && j + 4 < board[i].length && board[i][j] === board[i + 1][j + 1] && board[i + 1][j + 1] === board[i + 2][j + 2] && board[i + 2][j + 2] === board[i + 3][j + 3] && board[i][j] === board[i + 4][j + 4]) return board[i][j];
-				if(i + 4 < board.length && j - 4 < board[i].length && board[i][j] === board[i + 1][j - 1] && board[i + 1][j - 1] === board[i + 2][j - 2] && board[i + 2][j - 2] === board[i + 3][j - 3] && board[i][j] === board[i + 4][j - 4]) return board[i][j]
+				if(i + 4 < board.length && board[i][j] === board[i + 1][j] && board[i + 1][j] === board[i + 2][j] && board[i + 2][j] === board[i + 3][j] && board[i + 3][j] === board[i + 4][j]) return board[i][j];
+				if(j + 4 < board[i].length && board[i][j] === board[i][j + 1] && board[i][j + 1] === board[i][j + 2] && board[i][j + 2] === board[i][j + 3] && board[i][j + 3] === board[i][j + 4]) return board[i][j];
+				if(i + 4 < board.length && j + 4 < board[i].length && board[i][j] === board[i + 1][j + 1] && board[i + 1][j + 1] === board[i + 2][j + 2] && board[i + 2][j + 2] === board[i + 3][j + 3] && board[i + 3][j + 3] === board[i + 4][j + 4]) return board[i][j];
+				if(i + 4 < board.length && j - 4 < board[i].length && board[i][j] === board[i + 1][j - 1] && board[i + 1][j - 1] === board[i + 2][j - 2] && board[i + 2][j - 2] === board[i + 3][j - 3] && board[i + 3][j - 3] === board[i + 4][j - 4]) return board[i][j]
 			}
 		}
 		return undefined;
